7c914bb11f356423f6e68793559e23b62e7d3d0f
Nguyen Huu Quuan <quannguyenhuuit@gmail.com>
2017-07-05 15:37:52 +0700

    hoan chinh

--- BaiThi1/ViewController.swift
+++ BaiThi1/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func excuteFunction(_ sender: Any) {
+        let temp1 = 0
+        let temp2 = 100
+        let temp3 = 1000
+        let concurrentQueue = DispatchQueue(label: "queuename", attributes: .concurrent)
+        concurrentQueue.async {
+            for i in 0...9{
+                print( "🔴",temp1 + i)
+                print("🔵" ,temp2 + i)
+                print("⚫",temp3 + i)
+            }
+        }
+    }
+    @IBAction func excute2(_ sender: Any) {
+        let temp1 = 0
+        let temp2 = 100
+        let temp3 = 1000
+        for i in 0...9{
+            print( "🔴",temp1 + i)
+            print("🔵" ,temp2 + i)
+            print("⚫",temp3 + i)
+        }
+    }
 }
 
